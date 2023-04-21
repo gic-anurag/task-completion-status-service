38dc6cb116d13dcb73c4fc4fb30652995ac8e532
Anurag kumar singh <anurag.singh@gridinfocom.com>
2023-04-21 16:37:27 +0530

    added all three api's

--- task_status_service.go
+++ task_status_service.go
@@ -55,102 +55,65 @@ func (server Server) AddTaskCompletionStatus(taskStatusReq *TaskCompletionStatus
 	return &result, nil
 }
 
-// func (server *Server) UpdateHistory(journalId string, history *History) (*Response, error) {
-// 	var result Response
-// 	var buf bytes.Buffer
-// 	historyReq := map[string]interface{}{
-// 		"id":      journalId,
-// 		"history": history,
-// 	}
-// 	err := json.NewEncoder(&buf).Encode(historyReq)
+func (server Server) FilterTaskCompletionStatus(searchFilter map[string]interface{}) (*Response, error) {
+	var result Response
+	// payload := fmt.Sprint(searchFilter)
+	var buf bytes.Buffer
 
-// 	if err != nil {
-// 		return &result, err
-// 	}
+	err := json.NewEncoder(&buf).Encode(searchFilter)
 
-// 	client := &http.Client{}
-// 	req, err := http.NewRequest("POST", server.Server+"/journal/update/journal_history", strings.NewReader(buf.String()))
+	if err != nil {
+		return &result, err
+	}
 
-// 	if err != nil {
-// 		return &result, err
-// 	}
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", server.Server+"/api/filter/task-status", strings.NewReader(buf.String()))
+	if err != nil {
+		return &result, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return &result, err
+	}
 
-// 	req.Header.Add("Content-Type", "application/json")
+	defer resp.Body.Close()
 
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return &result, err
-// 	}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return &result, err
+	}
+	return &result, nil
+}
 
-// 	defer resp.Body.Close()
+func (server Server) GetTaskCompletionStatusById(taskStatusId *GetTaskCompletionStatus) (*Response, error) {
+	var result Response
+	var buf bytes.Buffer
 
-// 	err = json.NewDecoder(resp.Body).Decode(&result)
-// 	if err != nil {
-// 		return &result, err
-// 	}
+	err := json.NewEncoder(&buf).Encode(taskStatusId)
 
-// 	return &result, nil
-// }
+	if err != nil {
+		return &result, err
+	}
 
-// func (server Server) SearchFilter(searchFilter map[string]interface{}) (*Response, error) {
-// 	var result Response
-// 	// payload := fmt.Sprint(searchFilter)
-// 	var buf bytes.Buffer
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", server.Server+"/api/get-by-id/task-status", strings.NewReader(buf.String()))
 
-// 	err := json.NewEncoder(&buf).Encode(searchFilter)
+	if err != nil {
+		return &result, err
+	}
+	req.Header.Add("Content-Type", "application/json")
 
-// 	if err != nil {
-// 		return &result, err
-// 	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return &result, err
+	}
 
-// 	client := &http.Client{}
-// 	req, err := http.NewRequest("POST", server.Server+"/journal/filter/journal_entry", strings.NewReader(buf.String()))
-// 	if err != nil {
-// 		return &result, err
-// 	}
-// 	req.Header.Add("Content-Type", "application/json")
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return &result, err
-// 	}
+	defer resp.Body.Close()
 
-// 	defer resp.Body.Close()
-
-// 	err = json.NewDecoder(resp.Body).Decode(&result)
-// 	if err != nil {
-// 		return &result, err
-// 	}
-// 	return &result, nil
-// }
-
-// func (server Server) SearchByJournalId(journalId *FilterById) (*Response, error) {
-// 	var result Response
-// 	var buf bytes.Buffer
-
-// 	err := json.NewEncoder(&buf).Encode(journalId)
-
-// 	if err != nil {
-// 		return &result, err
-// 	}
-
-// 	client := &http.Client{}
-// 	req, err := http.NewRequest("POST", server.Server+"/journal/find/journal_entry_by_id", strings.NewReader(buf.String()))
-
-// 	if err != nil {
-// 		return &result, err
-// 	}
-// 	req.Header.Add("Content-Type", "application/json")
-
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return &result, err
-// 	}
-
-// 	defer resp.Body.Close()
-
-// 	err = json.NewDecoder(resp.Body).Decode(&result)
-// 	if err != nil {
-// 		return &result, err
-// 	}
-// 	return &result, nil
-// }
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return &result, err
+	}
+	return &result, nil
+}
